40297df24bf66070a9fbfbee6d0994f278f2b8ab
Da Moose <ahmed.musawir@hotmail.com>
2016-07-31 22:57:22 +0800

    Retreat Page - 31 July 2016

---
+++ template-retreat-page.php
@@ -0,0 +1,209 @@
+<?php
+/* Template Name: Retreat Page Template
+** page.php
+** mk_build_main_wrapper : builds the main divisions that contains the content. Located in framework/helpers/global.php
+** mk_get_view gets the parts of the pages, modules and components. Function located in framework/helpers/global.php
+*/
+
+get_header();
+
+
+// Mk_Static_Files::addAssets('mk_button'); 
+// Mk_Static_Files::addAssets('mk_audio');
+// Mk_Static_Files::addAssets('mk_swipe_slideshow');
+
+// mk_build_main_wrapper( mk_get_view('singular', 'wp-page', true) );
+
+?>
+
+<!--=======================================
+=   Da Moose is Loose Work With Me Page   =
+========================================-->
+
+
+<section id="book-retreat-block-1" class="">
+
+	<figure class="content-wrapper">
+
+		<div class="headline text-center wow zoomIn" data-wow-duration="2s" data-wow-delay="0s">
+		<i class="fa fa-fire fa-3x" aria-hidden="true"></i>
+			<h1 class="title">Authentic Abundance™ – A Weekend Retreat </h1>
+			<h1 class="details">
+				with best-selling author and money coach, Leisa Peterson
+			</h1>
+
+			<h2>October 7, 8, 9 in majestic Sedona, Arizona</h2>
+			<h2>You deserve the opportunity to free your mind and live more abundantly than ever before.</h2>
+
+			<h5>
+				Best-selling author and money coach Leisa Peterson will lead a transformational weekend retreat where you will explore your relationship with money while experiencing profound emotional relief, inner peace and accessing states of self-compassion, prosperity and joy.  Through presentations, discussions, energy work, and questions and answers, Leisa will guide you on an unprecedented journey of self-discovery and self-realization.
+			</h5>
+
+		</div>
+
+	</figure>	
+
+</section>
+
+<section id="book-retreat-block-2" class="">
+
+    <div class="container-fluid">
+	    	
+		<div class="quote-content">
+
+			<i class="fa fa-quote-left fa-5x" aria-hidden="true"></i>
+			<blockquote>
+				<h2>
+					Leisa lives a life of abundance. Her unique insights on bringing money and self-realization together into practical application empowers people to take big strides toward creating abundance in their own lives.
+				</h2>
+				<cite>
+					-- T. Harv Eker on Leisa’s work
+				</cite>				
+			</blockquote>
+
+
+		</div>
+		
+	</div>
+
+</section>
+
+<section id="book-retreat-block-3" class="">
+
+	<figure class="content-wrapper">
+
+		<div class="headline text-center zoomIn" data-wow-duration="2s" data-wow-delay="0s">
+			<i class="fa fa-leaf fa-3x" aria-hidden="true"></i>
+			<h1 class="title">THE RETREAT WILL TAKE PLACE AT THE FOLLOWING DATES & TIMES: </h1>
+			<!-- <h1 class="details">
+				with best-selling author and money coach, Leisa Peterson
+			</h1> -->
+
+
+			<h2>Friday, October 7th: 6 pm – 8 pm</h2>
+			<h2>Saturday, October 8th: 9 am – 5 pm</h2>
+			<h2>Sunday, October 9th: 9 am – 4 pm</h2>
+
+			<h5>
+				(Event includes vegetarian lunches on Saturday and Sunday, pre-retreat webinars, exercises, membership to a retreat group Facebook page where you can find room share options and make friendships before attending event, PLUS 2 follow up webinars to continue the integration of what you’ve learned at the retreat).
+			</h5>
+
+		</div>
+
+		<div id="workshop" class="headline text-center zoomIn" data-wow-duration="2s" data-wow-delay="0s">
+			<!-- <i class="fa fa-leaf fa-3x" aria-hidden="true"></i> -->
+			<h1 class="title">DURING THIS WORKSHOP, YOU WILL: </h1>
+
+			<h2>
+				<i class="fa fa-shield" aria-hidden="true"></i>
+				Receive direct instruction and healing practices from Leisa Peterson
+			</h2>
+			<h2>
+				<i class="fa fa-shield" aria-hidden="true"></i>
+				Free yourself from patterns of limitation and fear
+			</h2>
+			<h2>
+				<i class="fa fa-shield" aria-hidden="true"></i>
+				Learn to explore your relationship with money in a loving and allowing way
+			</h2>
+			<h2>
+				<i class="fa fa-shield" aria-hidden="true"></i>
+				Learn how to transform your greatest money challenges into your greatest teachings
+			</h2>
+			<h2>
+				<i class="fa fa-shield" aria-hidden="true"></i>
+				Develop tools for expanding your ability to give and receive
+			</h2>
+			<h2>
+				<i class="fa fa-shield" aria-hidden="true"></i>
+				Gain tools to transform your money so you can live in authentic abundance everyday
+			</h2>
+
+
+		</div>		
+
+	</figure>	
+
+</section>
+
+<section id="book-retreat-block-4" class="">
+
+	<figure class="content-wrapper">
+
+		<div class="headline text-center wow zoomIn" data-wow-duration="2s" data-wow-delay="0s">
+			<i class="fa fa-tree fa-3x" aria-hidden="true"></i>
+			<h1 class="title">COST:  $495 Early Bird until September 15th, $625 thereafter</h1>
+			<h1 class="details">
+				Leisa is hosting two FREE virtual webinars (August 1st and August 17th at 5 pm Pacific Time) and two in-person workshops in Sedona at the Sedona Creative Life Center on August 24th and September 6th, and will be offering special 2 for 1 discounts during those events.  
+			</h1>
+
+			<h5>
+				<a class="btn btn-success btn-lg" href="https://sp185.infusionsoft.com/app/manageCart/showManageOrder" target="_blank" title="">CLICK HERE TO REGISTER</a>				
+			</h5>
+
+			<h2><i class="fa fa-send" aria-hidden="true"></i>Contact Leisa at lpeterson@wealthclinic.com to sign up for the free workshops.</h2>
+
+
+		</div>
+
+	</figure>	
+
+</section>
+
+<section id="book-retreat-block-5" class="">
+
+    <div class="container-fluid">
+	    	
+		<div class="quote-content">
+
+			<i class="fa fa-quote-left fa-5x" aria-hidden="true"></i>
+			<blockquote>
+				<h2>
+					One of the best things I found in working with Leisa Peterson is the attention given to beliefs around money. Those (hidden) beliefs effect my everyday life, and with this new awareness, I feel well-armed against unconscious, limiting beliefs. With Leisa’s guidance and mindfulness direction, I feel like my new, successful relationship with money has totally opened up. Beliefs of all kinds can create openings or blocks; money is no different. I’m so excited to see some of my most strongly-held money beliefs be knocked down, creating a new opening for abundance.
+				</h2>
+				<cite>
+					-- Gretchen Breuner (From a past workshop attendee)
+				</cite>				
+			</blockquote>
+
+		</div>
+		<div class="leisa-bio clearfix">
+			<h3>
+				Leisa Peterson
+			</h3>
+			<p>
+				<img class="img-responsive pull-right" src="http://www.leisapeterson.com/wp-content/uploads/2016/07/final-2.png" alt="">
+				Leisa Peterson is an empath, money coach, life coach to entrepreneurs, author, podcaster and blogger.  She is known for her ability to quickly identify the exact issues that are holding a person back from their greatness, especially with money and wealth creation.  Once discovered Leisa draws upon various modalities, including shadow work, Emotional Freedom Technique, Allowing™, mindfulness practices, and breath work to assist her clients in clearing out these emotional and mental obstacles.  Leisa can then provide customized instructions for how to apply these breakthrough experiences into one’s practical life and business.
+			</p>
+			<p>
+				As a result of her courses, coaching and teachings, Leisa is helping thousands of people develop tools and skills for attracting greater wealth, freedom and peace of mind into their lives.  Leisa is the founder of WealthClinic, a CERTIFIED FINANCIAL PLANNER™, has an MBA in finance and worked in financial services for over two decades.  
+				<img class="img-responsive pull-left" src="http://www.leisapeterson.com/wp-content/uploads/2016/07/final-1.png" alt="">
+			</p>
+			<p>
+				Leisa did her best to avoid embracing her profound empathic abilities until several back-to-back tragedies led her to begin a journey to find the answers to questions like, “Why are we here?”, “Do we ever really die?”, “What is happiness?” This search led to exploring the world’s religions and working with many different teachers over the course of 17 years including Buddhism, Contemplative Christianity, Sufism, Hinduism,  the Tao, Self-Enquiry and more.  In addition to discovering the truths that exist beyond the physical world and any one religion, Leisa began to harness the incredible powers that arise when we tap into our intuitive connection with Source.  Leisa uses her intuitive abilities and familiarity of money challenges to help people break free of lack and scarcity patterns so they can fully step into their natural states of prosperity, joy and freedom.
+				<img class="img-responsive pull-right" src="http://www.leisapeterson.com/wp-content/uploads/2016/07/final-3.png" alt="">
+			</p>
+			<p>
+				Have questions about Leisa Peterson and the Authentic Abundance Retreat?  Please contact Leisa directly at 530-448-7081 or lpeterson@wealthclinic.com
+			</p>
+			<h3>
+				Retreat Overview:
+			</h3>
+			<h4><span class="day-time">Friday – October 7 – 6 – 8 pm</span> – Opening Ceremony. Start your journey with a powerful letting go practice and intention setting for the weekend retreat.</h4>
+
+			<h4><span class="day-time">Saturday – October 8 – 9 am to 5 pm</span> – AM Diving into Your Relationship with Money and Breaking Free from the Past.  Group Lunch.  PM with Leisa guiding meditations, facilitating group exercises and discussions.</h4>
+
+			<h4><span class="day-time">Sunday – October 9  – 9 am – 4 pm</span> – AM Trying on our new pair of wings for flying to new heights in our ability to receive.  Group Lunch.  PM with Leisa guided meditations and facilitating group exercises.  Final wrap up with Leisa.</h4>
+		
+		</div>
+
+	</div>
+
+</section>
+<!--====  End of Da Moose is Loose  ==== -->
+
+<?php
+
+get_footer();
+
+
